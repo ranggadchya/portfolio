f4c845a71b234aea1981b75a10964a66e94b0448
repteam <rangga.dwichya@gmail.com>
2022-04-04 23:31:03 +0700

    portfolio upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#0ea5e9',
+        dark: '#0f172a',
+        secondary: '#64748b',
+      },
+      gridTemplateColumns: {
+        footer: '150px minmax(0, 1fr)',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
