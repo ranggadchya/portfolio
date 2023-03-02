94bc5b378a101e8900082c8a0a6db86c78d1f1b4
ranggadchya <rangga.dwichya@gmail.com>
2023-03-02 10:02:05 +0700

    fix: user interface

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,25 @@
 module.exports = {
-  content: ['index.html'],
-  darkMode: 'class',
+  content: ["index.html"],
+  darkMode: "class",
   theme: {
+    fontFamily: {
+      montserrat: "Montserrat",
+    },
     container: {
       center: true,
-      padding: '16px',
+      padding: "16px",
     },
     extend: {
       colors: {
-        primary: '#0ea5e9',
-        dark: '#0f172a',
-        secondary: '#64748b',
+        primary: "#0ea5e9",
+        dark: "#0f172a",
+        secondary: "#64748b",
       },
       gridTemplateColumns: {
-        footer: '150px minmax(0, 1fr)',
+        footer: "150px minmax(0, 1fr)",
       },
       screens: {
-        '2xl': '1320px',
+        "2xl": "1320px",
       },
     },
   },
